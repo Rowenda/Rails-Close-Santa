3e70ae9020137ae3c518bba492662302ca5ee3e7
loic rosier <rosierloicdu14@gmail.com>
2021-12-13 21:40:58 +0100

    ajout des routes du controller reservations

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers
-  resources :reservations
+  resources :reservations, only: [:new, :create, :show, :destroy]
   #don't forget to except "edit and destroy" to nest with users routes
 end
