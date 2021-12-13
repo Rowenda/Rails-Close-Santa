f35106cd247ce2b4874a5b38e005dec8dcea16a7
loic rosier <rosierloicdu14@gmail.com>
2021-12-13 20:45:34 +0100

    ajout de commentaire au controller reservation + ajout de validates et a tester validates sur user

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,18 +1,27 @@
 class ReservationsController < ApplicationController
- # a faire method private before_action :find_user, only: [:create]
- # a faire method private before_action :find_offer, only: [:create]
+  # a faire method private before_action :find_user, only: [:create]
+  # a faire method private before_action :find_offer, only: [:create]
 
   def new
     @reservation = Reservation.new
   end
 
-  def create #pas tester
+  def create
+    # pas encore tester
+    # recupere l'user
     @user = User.find(params[:user_id])
+    # recupere l'offre
     @offer = Offer.find(params[:offer_id])
+    # assigne variable d'instance reservation nouvelle reservation avec params
+    # de la fonction privee reservation params
     @reservation = Reservation.new(reservation_params)
+    # assigne la valeur de l'id correspondant a l'user
     @reservation.user_id = @user
+    # assigne la valeur de l'id correspondant a l'offre
     @reservation.offer_id = @offer
+    # enregistre
     @reservation.save!
+    # attente des routes pour le if save redirect
   end
 
   def show
