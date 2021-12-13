3e2178c5000c4d203f272bf6b91149050f78a832
Rowenda <66358738+Rowenda@users.noreply.github.com>
2021-12-13 17:19:42 +0100

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :offers
+  #don't forget to except "edit and destroy" to nest with users routes
 end
