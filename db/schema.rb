c43393ed5b4a8c336256a81c8a2b31b3bf9fc30d
loic rosier <rosierloicdu14@gmail.com>
2021-12-13 17:36:50 +0100

    creation of table reservation wating on table offers

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_13_152904) do
+ActiveRecord::Schema.define(version: 2021_12_13_162956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reservations", force: :cascade do |t|
+    t.string "event_adress"
+    t.string "status"
+    t.time "start_time"
+    t.time "end_time"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_reservations_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_12_13_152904) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "reservations", "users"
 end
