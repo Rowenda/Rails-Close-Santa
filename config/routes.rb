1eb44ae5164327a3ca6bc4a2db98c428395c11a1
loic rosier <rosierloicdu14@gmail.com>
2021-12-14 14:30:20 +0100

    ajout des routes dans routes.rb et quelques link dans les views

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :offers, except: [:edit, :destroy]
-  #reservations a nester
-  #resources :reservations, only: [:new, :create, :show, :destroy]
-  
+  resources :offers, except: [:edit, :destroy, :dashboard] do
+    resources :reservations, only: [:new, :create]
+  end
+  resources :reservations, only: [:show, :destroy]
 
   #don't forget to except "edit and destroy" to nest with users routes
 end
