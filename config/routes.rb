4911b1d06df75b811f4e485e310dcc7d81216c91
loic rosier <rosierloicdu14@gmail.com>
2021-12-14 15:11:24 +0100

    ajout de la route dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,13 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   resources :offers, except: [:edit, :destroy] do
     resources :reservations, only: [:new, :create]
+    collection do
+      get :dasboard
+    end
   end
   resources :reservations, only: [:show, :destroy]
-  resources :offers, only: [:dashboard]
-
-  #don't forget to except "edit and destroy" to nest with users routes
+  # don't forget to except "edit and destroy" to nest with users routes
 end
