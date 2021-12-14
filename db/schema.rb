e03ac7a78059a7013b28c88bbe6718c32330b32b
baptiste giorgi <badbat_69@hotmail.com>
2021-12-14 10:32:38 +0100

    change for interface on my computer

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
-ActiveRecord::Schema.define(version: 2021_12_13_144009) do
+ActiveRecord::Schema.define(version: 2021_12_13_162956) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "offers", force: :cascade do |t|
+    t.date "availability"
+    t.string "location"
+    t.integer "price_per_hour"
+    t.text "offer_description"
+    t.string "title"
+    t.string "santa_description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "reservations", force: :cascade do |t|
     t.string "event_adress"
@@ -28,18 +36,6 @@ ActiveRecord::Schema.define(version: 2021_12_13_144009) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_reservations_on_user_id"
   end
-
-
-  create_table "offers", force: :cascade do |t|
-    t.date "availability"
-    t.string "location"
-    t.integer "price_per_hour"
-    t.text "offer_description"
-    t.string "title"
-    t.string "santa_description"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
