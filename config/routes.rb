a41f6a248896d39f091e8eaf638ff53a811b5718
loic rosier <rosierloicdu14@gmail.com>
2021-12-14 14:50:10 +0100

    changement de routes pour dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
-  resources :offers, except: [:edit, :destroy, :dashboard] do
+  resources :offers, except: [:edit, :destroy] do
     resources :reservations, only: [:new, :create]
   end
   resources :reservations, only: [:show, :destroy]
+  resources :offers, only: [:dashboard]
 
   #don't forget to except "edit and destroy" to nest with users routes
 end
