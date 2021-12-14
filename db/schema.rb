efef1dd6e674cc5ddfdb1f80a12ec390975b140d
baptiste giorgi <badbat_69@hotmail.com>
2021-12-14 16:47:06 +0100

    first seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_12_13_165624) do
+ActiveRecord::Schema.define(version: 2021_12_13_184448) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +55,7 @@ ActiveRecord::Schema.define(version: 2021_12_13_165624) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
-  add_foreign_key "reservations", "offers"
   add_foreign_key "offers", "users"
+  add_foreign_key "reservations", "offers"
   add_foreign_key "reservations", "users"
 end
