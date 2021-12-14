136c064bc0c173d5ada3e0f574f2d106b9eb290a
Pauline Dupuid <pauline.dupuid@gmail.com>
2021-12-14 13:02:57 +0100

    associations and validations added to user and reservations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+  has_many :reservations
+  has_many :offers
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
