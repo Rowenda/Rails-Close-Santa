a5cadcfbdb25ec7649afe8e55232a26b5f31e4ed
baptiste giorgi <badbat_69@hotmail.com>
2021-12-16 12:35:51 +0100

    aborted test

---
+++ db/migrate/20211216111835_change_reservation_start_time_and_end_time_name.rb
@@ -0,0 +1,6 @@
+class ChangeReservationStartTimeAndEndTimeName < ActiveRecord::Migration[6.1]
+  def change
+    rename_column :reservations, :start_time, :reservation_start
+    rename_column :reservations, :end_time, :reservation_end
+  end
+end
