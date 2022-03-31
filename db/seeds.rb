85e491edec2761778a7438aea2be579e9c92a0a5
baptiste giorgi <badbat_69@hotmail.com>
2022-03-31 15:41:31 +0200

    seed update

--- db/seeds.rb
+++ db/seeds.rb
@@ -35,7 +35,7 @@ puts "-----user created-----"
 
 puts "-----offer creation-----"
 
-image1 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639578231/development/nmxcoqy7rp1v84whnviabbivzf5n.jpg')
+image1 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831394/development/4q7h01bebitk9zs67w7ggq7ittq0.jpg')
 offers1 = Offer.new(location: "Nice", availability: Date.new(2021, 12, 24), price_per_hour: 80,
                     offer_description: "the best santa in Nice and around, go throw your chimney,
                     say HohoHo like nobody and adorable with the childrens",
@@ -46,7 +46,7 @@ offers1.user = johndoe
 offers1.photo.attach(io: image1, filename: 'johndoepic', content_type: 'image/png')
 offers1.save!
 
-image2 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639751069/development/santa2_tgitli.jpg')
+image2 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831398/development/m6jnobb1ukhpymzb42orp6mfd3i6.jpg')
 offers2 = Offer.new(location: "Cannes", availability: Date.new(2021, 12, 20), price_per_hour: 60,
                     offer_description: "If you need a good santa in Cannes, don't look further, I'm graduate from
                     the Santa school, clean, kind, and I had for you a great show.",
@@ -57,7 +57,7 @@ offers2.user = paulmaccartney
 offers2.photo.attach(io: image2, filename: 'paulpic', content_type: 'image/png')
 offers2.save!
 
-image3 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639580337/development/paul_cyeqep.jpg')
+image3 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831400/development/7a8iuha0pn1ekj80bractaz83d4w.jpg')
 offers3 = Offer.new(location: "Cannes", availability: Date.new(2021, 12, 25), price_per_hour: 90,
                     offer_description: "I can come for your christmas day to give your gift to your child, make their
                     christmas be a real and magical experience.",
@@ -68,7 +68,7 @@ offers3.user = paulmaccartney
 offers3.photo.attach(io: image3, filename: 'paulpic', content_type: 'image/png')
 offers3.save!
 
-image4 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639580336/development/romeo_f6n1wk.jpg')
+image4 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831410/development/99afb9k797z4ea3eqfz0m5uplax6.jpg')
 offers4 = Offer.new(location: "Marseille", availability: Date.new(2021, 12, 16), price_per_hour: 60,
                     offer_description: "You search a Santa for your christmas and I'm the older Santa of Marseille,
                     30 years of experience, sober, clean, I can take care of the wish of your familly !",
@@ -80,7 +80,7 @@ offers4.photo.attach(io: image4, filename: 'lennonpic', content_type: 'image/png
 offers4.save!
 
 
-image5 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639580336/development/lennon_zbmwbz.jpg')
+image5 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639852826/development/52kv1cyiz3v954ld10u7lizvd8ow.jpg')
 offers5 = Offer.new(location: "Marseille", availability: Date.new(2021, 12, 23), price_per_hour: 90,
                     offer_description: "Christmas approach, bring a magical gift to your childs with this unique
                     experience.",
@@ -91,7 +91,7 @@ offers5.user = lennon
 offers5.photo.attach(io: image5, filename: 'lennonpic', content_type: 'image/png')
 offers5.save!
 
-image6 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639750905/development/santa-in-a-hot-tub-2_nreyxt.jpg')
+image6 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831404/development/zb2b2ot26xlvuf0c8yqho1qpf08z.jpg')
 offers6 = Offer.new(location: "Marseille", availability: Date.new(2021, 12, 31), price_per_hour: 80,
                     offer_description: "What is best than a Santa for your Christmas ? Santa Claus can be here to your
                     new year, for particular, enterprise, bring some christmas magic after christmas and enjoy your
@@ -103,7 +103,7 @@ offers6.user = lennon
 offers6.photo.attach(io: image6, filename: 'lennonpic', content_type: 'image/png')
 offers6.save!
 
-image7 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639750959/development/hot-santa_hyc8by.jpg')
+image7 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831407/development/xn48bnvmwxr2jb76jjl8hi9btskt.jpg')
 offers7 = Offer.new(location: "Nice", availability: Date.new(2021, 12, 18), price_per_hour: 70,
                     offer_description: "On your holiday, choose the perfect Santa Claus to enjoy christmas time",
                     title: "Nice Santa",
@@ -112,7 +112,7 @@ offers7.user = romeo
 offers7.photo.attach(io: image7, filename: 'romeopic', content_type: 'image/png')
 offers7.save!
 
-image8 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639580336/development/santa_nippsl.jpg')
+image8 = URI.open('https://res.cloudinary.com/dn5lpurr4/image/upload/v1639831402/development/drhubdh9bjpltol0bpq5g9xs8yn9.jpg')
 offers8 = Offer.new(location: "France", availability: Date.new(2021, 12, 21), price_per_hour: 0,
                     offer_description: "Ho HO Ho it's almost christmas, don't forget to be a good child and make your
                     wishlist.",
